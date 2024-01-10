d004ec981171d8c236917774e3e5c8bb06315c63
Shohei Takeda <123245041+take0603@users.noreply.github.com>
2024-01-10 22:02:26 +0900

    blocked hostによるエラー回避のため修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts << "nomishiro-bancho-2b23287ae884.herokuapp.com"
 end
