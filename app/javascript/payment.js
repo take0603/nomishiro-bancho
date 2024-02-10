393d44e5cf4b37765f91461630536ae269f08c1f
Shohei Takeda <123245041+take0603@users.noreply.github.com>
2024-02-10 22:04:12 +0900

    支払表作成・編集に傾斜精算計算を追加

--- app/javascript/payment.js
+++ app/javascript/payment.js
@@ -5,7 +5,9 @@ const addButton = document.getElementById('addButton');
 addButton.addEventListener('click', function() {
   const table = document.getElementById('payment_table');
   const row = table.insertRow(-1);
+  row.classList.add('payment_row');
   const cell_participant = row.insertCell(-1);
+  const cell_ratio = row.insertCell(-1);
   const cell_fee = row.insertCell(-1);
   const cell_delete = row.insertCell(-1);
 
@@ -14,39 +16,48 @@ addButton.addEventListener('click', function() {
     <input type=text
     name=payment[payment_details_attributes][${i}][participant]
     id=payment_payment_details_attributes_${i}_participant>`;
+  let ratioForm = `
+    <select>
+      <optgroup label="多い"></optgroup>
+      <option value="5">5</option>
+      <option value="4">4</option>
+      <option value="3" selected>3</option>
+      <option value="2">2</option>
+      <option value="1">1</option>
+      <optgroup label="少ない"></optgroup>
+      <hr>
+      <option value="0">請求なし</option>
+    </select>`;
   let feeForm = `
     <label class=payment_label for=payment_payment_details_attributes_${i}_fee>支払額</label>
-    <input type=number value=0
+    <input class=payment_fee_form type=number value=0
     name=payment[payment_details_attributes][${i}][fee]
     id=payment_payment_details_attributes_${i}_fee>`;
   let deleteButton = `<span class="deleteButton">削除</span>`;
   cell_participant.innerHTML = nameForm;
+  cell_ratio.innerHTML = ratioForm;
   cell_fee.innerHTML = feeForm;
   cell_delete.innerHTML = deleteButton;
 
   addDeleteEvent();
+  addSumFeeEvent();
   i++ ;
 });
 
 // new edit 行(フォーム)削除
 addDeleteEvent();
 function addDeleteEvent() {
-  let deleteButton = document.querySelectorAll('.deleteButton');
+  const deleteButton = document.querySelectorAll('.deleteButton');
   deleteButton.forEach(function(button) {
-    if (window.location.pathname.includes('/new')) button.addEventListener('click', removeRow);
-    if (window.location.pathname.includes('/edit')) button.addEventListener('click', hiddenRow);
+    button.addEventListener('click', hiddenRow);
   });
-};
-
-function removeRow() {
-  const row = this.closest('tr');
-  row.remove();
 };
 
 function hiddenRow() {
   const row = this.closest('tr');
   row.style.display = 'none';
-  // accepts_nested_attributes_forでレコード削除のため_destroy属性を追加
+  row.className = 'payment_row_hidden';
+  // update時にaccepts_nested_attributes_forでDBレコード削除のため_destroy属性を追加
   let i = row.rowIndex - 1;
   let deleteAttribute = `
     <input value="true"
@@ -54,4 +65,70 @@ function hiddenRow() {
     name="payment[payment_details_attributes][${i}][_destroy]"
     id="payment_payment_details_attributes_${i}__destroy"></input>`;
   row.innerHTML += deleteAttribute;
+  sumFee();
+};
+
+//new edit 支払額計算・表示
+const calcButton = document.getElementById('calcButton');
+calcButton.addEventListener('click', function() {
+  let roundSelect = '';
+  let totalRatio = 0;
+  let amount = document.getElementById('payment_amount').value;
+
+  let elem = document.getElementsByName('round');
+  for (let i = 0; i < elem.length; i++) {
+    if (elem[i].checked) {
+      roundSelect = elem[i].value;
+      break;
+    };
+  };
+
+  let row = document.querySelectorAll('.payment_row');
+  row.forEach(function(row) {
+    let select = row.querySelector('select').value;
+    totalRatio += parseInt(select);
+  });
+  row.forEach(function(row) {
+    let ratio = row.querySelector('select').value;
+    let feeForm = row.querySelector('.payment_fee_form');
+    let fee = amount * (ratio / totalRatio);
+    switch(roundSelect){
+      case 'default' :
+        feeForm.value = Math.round(fee);
+        break;
+      case '100yen' :
+        feeForm.value = Math.round(fee / 100) * 100;
+        break;
+      case '500yen' :
+        feeForm.value = Math.round(fee / 500) * 500;
+        break;
+    };
+  });
+  sumFee();
+});
+
+// new edit　各メンバーの支払額合計・イベント総額に対する差額表示
+addSumFeeEvent();
+function addSumFeeEvent() {
+  let row = document.querySelectorAll('.payment_row');
+  row.forEach(function(row) {
+    let feeForm = row.querySelector('.payment_fee_form');
+    feeForm.addEventListener('change', sumFee);
+  });
+};
+
+function sumFee() {
+  let totalFee = 0;
+  let row = document.querySelectorAll('.payment_row');
+  let amount = document.getElementById('payment_amount').value;
+  row.forEach(function(row) {
+    let select = row.querySelector('.payment_fee_form').value;
+    totalFee += parseInt(select) || 0;
+  });
+  document.getElementById('sum_fee').innerHTML = totalFee;
+  document.getElementById('diff_fee').innerHTML = amount - totalFee;
+};
+
+if (window.location.pathname.includes('edit')) {
+  sumFee();
 };
