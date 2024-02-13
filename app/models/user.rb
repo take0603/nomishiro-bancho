39888cdaa22e02679b96493bd0e850c9a49bd7ab
Shohei Takeda <123245041+take0603@users.noreply.github.com>
2024-02-13 22:07:08 +0900

    ゲストユーザーとしてログイン機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#top'
   get '/mypage', to: 'home#mypage'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   get '/users/account', to: 'users#show'
   resources :events do
     resource :attendances
